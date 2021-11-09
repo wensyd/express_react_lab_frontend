bfea38f5ae3b058a1139595d270c2db63a2e80c9
wensydesousa <wensys013@gmail.com>
2021-11-09 11:56:26 -0800

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// IMPORT COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+// IMPORT PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 function App() {
+  //HEROKU URL from backend
+  const URL = "https://wd-express-react-lab.herokuapp.com/";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/projects" element={<Projects URL={URL}/>}/>
+        <Route exact path="/about" element={<About URL={URL}/>}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
